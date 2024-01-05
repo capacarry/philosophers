d648100ce9fd3a773bbe32bd5c582d46653d23dd
Guilherme Capa <125405371+capacarry@users.noreply.github.com>
2024-01-05 17:06:44 +0000

    Add files via upload

---
+++ philo/src/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: capa <capa@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/27 13:32:57 by capa              #+#    #+#             */
+/*   Updated: 2024/01/04 16:06:06 by capa             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*Parsing input, and start of program.*/
+int	main(int ac, char *av[])
+{
+	t_global_data	data;
+
+	if (ac >= 5 && ac <= 6)
+	{
+		if (check_args(av, ac))
+		{
+			if (data_init(&data, av, ac) == 1)
+			{
+				free(data.all_philosophers);
+				return (0);
+			}
+			if (philo_init(&data) == -1)
+				return (0);
+			init_threads(&data);
+			free_everything(&data);
+		}
+	}
+	else
+		printf("Wrong number of arguments ❌!\n");
+}
